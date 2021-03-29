2ceade96bd183e011e9d2a20b2d7f168b429d453
colemaxwell <cole.h.maxwell@gmail.com>
2021-03-29 16:07:22 -0400

    upload

--- src/cs681/StartupSportsStats.java
+++ src/cs681/StartupSportsStats.java
@@ -1,7 +1,39 @@
 package cs681;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 class StartupSportsStats {
     public static void main(String[] args) {
-        System.out.println("test");
+
+        ArrayList<Integer> nbaPPG = new ArrayList<Integer>();
+        try {
+            File PPG = new File("NBAPointsPerGame.txt");
+            Scanner scanner = new Scanner(PPG);
+            while (scanner.hasNextLine()) {
+              String strPoints = scanner.nextLine();
+              Integer intPoints = Integer.parseInt(strPoints);
+              nbaPPG.add(intPoints);
+            }
+            scanner.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+
+        Long countGames = nbaPPG.stream()
+            .filter( points -> points<100 )
+            .count();
+
+        System.out.println("--Number of NBA games out of " + nbaPPG.size() +
+                            " games\nwith total points per game above 100--");
+        System.out.println(countGames);
+                                
+                                                
+    
+                                    
     }
 }
